23d74aabcf05fcfbb3f43c4d465c881fbf16cf53
Joaquín Uribe <Frocoa@gmail.com>
2021-05-30 13:23:05 -0400

    agregado el cel shading y luces tipo spotlight

---
+++ Ejercicio 8.py
@@ -0,0 +1,293 @@
+""" P3] Se presenta una escena con 4 fuentes de luz, pipelines en newLightShaders.py """
+
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import grafica.transformations as tr
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
+import grafica.performance_monitor as pm
+import grafica.lighting_shaders as ls
+import grafica.scene_graph as sg
+from shapes3d import *
+import newLightShaders as nl
+
+# Clase para manejar una camara que se mueve en coordenadas polares
+class PolarCamera:
+    def __init__(self):
+        self.center = np.array([0.0, 0.0, -0.5]) # centro de movimiento de la camara y donde mira la camara
+        self.theta = 0                           # coordenada theta, angulo de la camara
+        self.rho = 5                             # coordenada rho, distancia al centro de la camara
+        self.eye = np.array([0.0, 0.0, 0.0])     # posicion de la camara
+        self.height = 0.5                        # altura fija de la camara
+        self.up = np.array([0, 0, 1])            # vector up
+        self.viewMatrix = None                   # Matriz de vista
+    
+    # Añadir ángulo a la coordenada theta
+    def set_theta(self, delta):
+        self.theta = (self.theta + delta) % (np.pi * 2)
+
+    # Añadir distancia a la coordenada rho, sin dejar que sea menor o igual a 0
+    def set_rho(self, delta):
+        if ((self.rho + delta) > 0.1):
+            self.rho += delta
+    
+    # Actualizar la matriz de vista
+    def update_view(self):
+        # Se calcula la posición de la camara con coordenadas poleras relativas al centro
+        self.eye[0] = self.rho * np.sin(self.theta) + self.center[0]
+        self.eye[1] = self.rho * np.cos(self.theta) + self.center[1]
+        self.eye[2] = self.height + self.center[2]
+
+        # Se genera la matriz de vista
+        viewMatrix = tr.lookAt(
+            self.eye,
+            self.center,
+            self.up
+        )
+        return viewMatrix
+
+# Clase para manejar el controlador y la camara polar
+class Controller:
+    def __init__(self):
+        self.fillPolygon = True
+        self.showAxis = True
+
+        # Variables para controlar la camara
+        self.is_up_pressed = False
+        self.is_down_pressed = False
+        self.is_left_pressed = False
+        self.is_right_pressed = False
+
+        # Se crea instancia de la camara
+        self.polar_camera = PolarCamera()
+
+    # Entregar la referencia a la camara
+    def get_camera(self):
+        return self.polar_camera
+
+    # Metodo para ller el input del teclado
+    def on_key(self, window, key, scancode, action, mods):
+
+        # Caso de detectar la tecla [UP], actualiza estado de variable
+        if key == glfw.KEY_UP:
+            if action == glfw.PRESS:
+                self.is_up_pressed = True
+            elif action == glfw.RELEASE:
+                self.is_up_pressed = False
+
+        # Caso de detectar la tecla [DOWN], actualiza estado de variable
+        if key == glfw.KEY_DOWN:
+            if action == glfw.PRESS:
+                self.is_down_pressed = True
+            elif action == glfw.RELEASE:
+                self.is_down_pressed = False
+
+        # Caso de detectar la tecla [RIGHT], actualiza estado de variable
+        if key == glfw.KEY_RIGHT:
+            if action == glfw.PRESS:
+                self.is_right_pressed = True
+            elif action == glfw.RELEASE:
+                self.is_right_pressed = False
+
+        # Caso de detectar la tecla [LEFT], actualiza estado de variable
+        if key == glfw.KEY_LEFT:
+            if action == glfw.PRESS:
+                self.is_left_pressed = True
+            elif action == glfw.RELEASE:
+                self.is_left_pressed = False
+        
+        # Caso de detectar la barra espaciadora, se cambia el metodo de dibujo
+        if key == glfw.KEY_SPACE:
+            if action == glfw.PRESS:
+                self.fillPolygon = not self.fillPolygon
+
+        # Caso en que se cierra la ventana
+        if key == glfw.KEY_ESCAPE:
+            if action == glfw.PRESS:
+                glfw.set_window_should_close(window, True)
+
+        # Caso de detectar Control izquierdo, se cambia el metodo de dibujo
+        elif key == glfw.KEY_LEFT_CONTROL:
+            if action == glfw.PRESS:
+                self.showAxis = not self.showAxis
+
+    #Funcion que recibe el input para manejar la camara y controlar sus coordenadas
+    def update_camera(self, delta):
+        # Camara rota a la izquierda
+        if self.is_left_pressed:
+            self.polar_camera.set_theta(-2 * delta)
+
+        # Camara rota a la derecha
+        if self.is_right_pressed:
+            self.polar_camera.set_theta( 2 * delta)
+        
+        # Camara se acerca al centro
+        if self.is_up_pressed:
+            self.polar_camera.set_rho(-5 * delta)
+
+        # Camara se aleja del centro
+        if self.is_down_pressed:
+            self.polar_camera.set_rho(5 * delta)
+
+if __name__ == "__main__":
+
+    # Initialize glfw
+    if not glfw.init():
+        glfw.set_window_should_close(window, True)
+
+    width = 800
+    height = 800
+    title = "P0 - Scene"
+
+    window = glfw.create_window(width, height, title, None, None)
+
+    if not window:
+        glfw.terminate()
+        glfw.set_window_should_close(window, True)
+
+    glfw.make_context_current(window)
+
+    controller = Controller()
+    # Connecting the callback function 'on_key' to handle keyboard events
+    glfw.set_key_callback(window, controller.on_key)
+
+    # Pipeline con shaders con multiples fuentes de luz
+    phongPipeline = nl.MultiplePhongShaderProgram()
+    phongSpotPipeline = nl.MultiplePhongSpotShaderProgram()
+    phongTexPipeline = nl.MultipleTexturePhongShaderProgram()
+
+    # This shader program does not consider lighting
+    mvpPipeline = es.SimpleModelViewProjectionShaderProgram()
+
+    # Setting up the clear screen color
+    glClearColor(0.85, 0.85, 0.85, 1.0)
+
+    # As we work in 3D, we need to check which part is in front,
+    # and which one is at the back
+    glEnable(GL_DEPTH_TEST)
+
+    # Creating shapes on GPU memory
+    gpuAxis = createGPUShape(mvpPipeline, bs.createAxis(4))
+
+    scene = createScene(phongPipeline)
+    toro1 = createToroNode(0.7, 0.6, 0.4, phongPipeline)
+    tex_toro = createTexToroNode(phongTexPipeline)
+
+    					  #Color         
+    colormat = np.array([[1.0,1.0,1.0,],
+    					 [0.0,1.0,1.0,],
+    					 [1.0,1.0,0.0,]])
+
+    colorIndex = 0
+
+    perfMonitor = pm.PerformanceMonitor(glfw.get_time(), 0.5)
+    # glfw will swap buffers as soon as possible
+    glfw.swap_interval(0)
+    t0 = glfw.get_time()
+    t_inicial = glfw.get_time()
+
+    # Application loop
+    while not glfw.window_should_close(window):
+        # Variables del tiempo
+        t1 = glfw.get_time()
+        delta = t1 -t0
+        t0 = t1
+        variacion = t1 - t_inicial
+        colorVec = colormat[colorIndex]
+
+        if variacion >= 4.0:
+        	colorIndex = (colorIndex+1)%3
+        	t_inicial = t1
+
+        # Using GLFW to check for input events
+        glfw.poll_events()
+
+        controller.update_camera(delta)
+        camera = controller.get_camera()
+        viewMatrix = camera.update_view()
+
+        # Setting up the projection transform
+        projection = tr.perspective(60, float(width) / float(height), 0.1, 100)
+
+        # Clearing the screen in both, color and depth
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Filling or not the shapes depending on the controller state
+        if (controller.fillPolygon):
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        # The axis is drawn without lighting effects
+        if controller.showAxis:
+            glUseProgram(mvpPipeline.shaderProgram)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"), 1, GL_TRUE, viewMatrix)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+            mvpPipeline.drawCall(gpuAxis, GL_LINES)
+
+        lightingPipeline = phongPipeline
+        lightingPipeline = phongSpotPipeline
+
+        # Setting all uniform shader variables
+        
+        glUseProgram(lightingPipeline.shaderProgram)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "La"), colorVec[0]*0.25, colorVec[1]*0.25, colorVec[2]*0.25)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ld"), colorVec[0]*0.5, colorVec[1]*0.5, colorVec[2]*0.5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ls"), colorVec[0], colorVec[1], colorVec[2])
+
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ka"), colorVec[0]*0.2, colorVec[1]*0.2, colorVec[2]*0.2)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Kd"), colorVec[0]*0.5, colorVec[1]*0.5, colorVec[2]*0.5)
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "Ks"), colorVec[0], colorVec[1], colorVec[2])
+
+        # Ya no se necesita la posicion de la fuentes de luz, se declaran constantes en los shaders
+        glUniform3f(glGetUniformLocation(lightingPipeline.shaderProgram, "viewPosition"), camera.eye[0], camera.eye[1], camera.eye[2])
+        
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "constantAttenuation"), 0.01)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "quadraticAttenuation"), 0.05)
+        glUniform1f(glGetUniformLocation(lightingPipeline.shaderProgram, "linearAttenuation"), 0.03)
+        
+
+        glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "view"), 1, GL_TRUE, viewMatrix)
+
+        # Drawing
+        sg.drawSceneGraphNode(scene, lightingPipeline, "model",20)
+        sg.drawSceneGraphNode(toro1, lightingPipeline, "model",100,0.05)
+        
+
+        # Se dibuja con el pipeline de texturas
+        glUseProgram(phongTexPipeline.shaderProgram)
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "La"), colorVec[0]*0.25, colorVec[1]*0.25, colorVec[2]*0.25)
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "Ld"), colorVec[0]*0.5, colorVec[1]*0.5, colorVec[2]*0.5)
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "Ls"), colorVec[0], colorVec[1], colorVec[2])
+
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "Ka"), colorVec[0]*0.2, colorVec[1]*0.2, colorVec[2]*0.2)
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "Kd"), colorVec[0]*0.5, colorVec[1]*0.5, colorVec[2]*0.5)
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "Ks"), colorVec[0], colorVec[1], colorVec[2])
+
+        # Ya no se necesita la posicion de la fuentes de lus, se declaran constantes en los shaders
+        glUniform3f(glGetUniformLocation(phongTexPipeline.shaderProgram, "viewPosition"), camera.eye[0], camera.eye[1], camera.eye[2])
+        glUniform1ui(glGetUniformLocation(phongTexPipeline.shaderProgram, "shininess"), 100)
+        
+        glUniform1f(glGetUniformLocation(phongTexPipeline.shaderProgram, "constantAttenuation"), 0.001)
+        glUniform1f(glGetUniformLocation(phongTexPipeline.shaderProgram, "linearAttenuation"), 0.03)
+        glUniform1f(glGetUniformLocation(phongTexPipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+        glUniformMatrix4fv(glGetUniformLocation(phongTexPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(phongTexPipeline.shaderProgram, "view"), 1, GL_TRUE, viewMatrix)
+
+        sg.drawSceneGraphNode(tex_toro, phongTexPipeline, "model", 50,0.05)
+        
+        
+        # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
+        glfw.swap_buffers(window)
+
+    gpuAxis.clear()
+    scene.clear()
+    toro1.clear()
+    tex_toro.clear()
+
+    glfw.terminate()
