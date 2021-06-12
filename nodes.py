4ef76cc37013925fa19dbb3bc3a004fe01380ab9
Frocoa <Frocoa@gmail.com>
2021-06-12 16:11:11 -0400

    Espinas del personaje

--- nodes.py
+++ nodes.py
@@ -7,13 +7,21 @@ from shapes3d import *
 def createTail(pipeline):
     tailMesh = mh.createTailMesh()
     tailGPU = createGPUShape(pipeline, mh.toShape(tailMesh, color=(255/255, 128/255, 0.0)))
+    spikeGPU = createGPUShape(pipeline, mh.toShape(tailMesh, color = (220/255, 220/255, 220/225)))
+
 
     tail4 = GameObject("tail4", pipeline)
     tail4.setModel(tailGPU)
     tail4.setPosition([0, 0, 1])
 
+    spike4 = GameObject("spike4", pipeline)
+    spike4.setModel(spikeGPU)
+    spike4.uniformScale(0.3)
+    spike4.setRotation([0, 90, 0])
+    spike4.setPosition([0.7, 0, 0.5])
+
     joint4 = GameObject("joint4", pipeline)
-    joint4.addChilds([tail4]) 
+    joint4.addChilds([tail4, spike4]) 
     joint4.setPosition([0, 0, 0.5])
     joint4. setRotation([0, 15, 0])
 
@@ -21,8 +29,14 @@ def createTail(pipeline):
     tail3.setModel(tailGPU)
     tail3.setPosition([0, 0, 1])
 
+    spike3 = GameObject("spike3", pipeline)
+    spike3.setModel(spikeGPU)
+    spike3.uniformScale(0.3)
+    spike3.setRotation([0, 90, 0])
+    spike3.setPosition([0.7, 0, 0.5])
+
     joint3 = GameObject("joint3", pipeline)
-    joint3.addChilds([tail3, joint4]) 
+    joint3.addChilds([tail3, spike3, joint4]) 
     joint3.setPosition([0, 0, 0.5])
     joint3. setRotation([0, 15, 0])
     
@@ -30,8 +44,14 @@ def createTail(pipeline):
     tail2.setModel(tailGPU)
     tail2.setPosition([0, 0, 1])
 
+    spike2 = GameObject("spike2", pipeline)
+    spike2.setModel(spikeGPU)
+    spike2.uniformScale(0.3)
+    spike2.setRotation([0, 90, 0])
+    spike2.setPosition([0.7, 0, 0.5])
+
     joint2 = GameObject("joint2", pipeline)
-    joint2.addChilds([tail2, joint3])
+    joint2.addChilds([tail2, spike2, joint3])
     joint2.setPosition([0, 0, 0.5])
     joint2.setRotation([0, 15, 0])
 
@@ -39,8 +59,14 @@ def createTail(pipeline):
     tail1.setModel(tailGPU)
     tail1.setPosition([0, 0, 1])
 
+    spike1 = GameObject("spike1", pipeline)
+    spike1.setModel(spikeGPU)
+    spike1.uniformScale(0.3)
+    spike1.setRotation([0, 90, 0])
+    spike1.setPosition([0.7, 0, 0.5])
+
     joint1 = GameObject("joint1", pipeline)
-    joint1.addChilds([tail1, joint2])
+    joint1.addChilds([tail1, spike1, joint2])
     joint1.setPosition([-1.4,0,0])
     joint1.setRotation([0,-90,0])
 
